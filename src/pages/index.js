bb095ccc3704a4714915384c9803cd0f4443db5a
musab <musab.yaa@gmail.com>
2022-06-20 09:26:53 +0300

    Save index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import Layout from '../components/Layout/Layout'
 
-export default function Home() {
-  return <Layout>Main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return (
+    <Layout>
+      <div>Found {countries.length} countries</div>
+    </Layout>
+  );
 }
 // API in the NEXT JS
 export const getStaticProps = async () => {
-  const res = await fetch("")
+  const res = await fetch("https://restcountries.eu/rest/v2/all")
   const countries = res.json();
   return { // key-value pair
     props: {
